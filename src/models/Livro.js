25d409ee1770b3533ba5165ec577ba992a12e586
MarcosRBjunior <marcosribeirojr@alunos.fho.edu.br>
2026-06-16 22:23:44 -0300

    feat(models): customize validation messages using {VALUE} in Livro

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -5,8 +5,8 @@ const livroSchema = new mongoose.Schema(
     id: {type: String},
     titulo: {type: String, required: [true, "O título do livro é obrigatório"]},
     autor: {type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: [true, "O autor é obrigatório"]},
-    editora: {type: String, required: [true, "A editora é obrigatória"]},
-    numeroPaginas: {type: Number}
+    editora: {type: String, required: [true, "A editora é obrigatória"], enum: { values: ["Dicionario do autodidata", "Bordeslesscoding"], message: "A editora {VALUE} não é válida"}},
+    numeroPaginas: {type: Number, min: [10, "O numero de paginas deve estar entre 10 e 5000 {VALUE}"], max: [5000, "O numero de paginas deve estar entre 10 e 5000 {VALUE}"]},
   }
 );
 
