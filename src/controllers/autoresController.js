3deef23a35077e039f458dd0e14cfd678f0040ca
MarcosRBjunior <marcosribeirojr@alunos.fho.edu.br>
2026-06-16 21:53:41 -0300

    feat: centralize API error management

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -57,13 +57,17 @@ class AutorController {
   
     static excluirAutor = async (req, res, next) => {
       try {
-        const id = req.params.id;
-  
-        await autores.findByIdAndDelete(id);
-  
-        res.status(200).send({message: "Autor removido com sucesso"});
+       const id = req.params.id;
+
+       const autorResultado = await autores.findByIdAndDelete(id);
+
+       if (autorResultado !== null) {
+         res.status(200).send({message: "Autor removido com sucesso"});
+        } else {
+        next(new NaoEncontrado("Id do Autor não localizado."));
+        }
       } catch (erro) {
-        next(erro);
+      next(erro);
       }
     }
   
--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,3 +1,4 @@
+import NaoEncontrado from "../erros/NaoEncontrado.js";
 import livros from "../models/Livro.js";
 
 class LivroController {
@@ -15,19 +16,23 @@ class LivroController {
     }
   }
 
-  static listarLivroPorId = async (req, res, next) => {
-    try {
-      const id = req.params.id;
+    static listarLivroPorId = async (req, res, next) => {
+      try {
+        const id = req.params.id;
 
-      const livroResultados = await livros.findById(id)
-        .populate("autor", "nome")
-        .exec();
+        const livroResultado = await livros.findById(id)
+         .populate("autor", "nome")
+          .exec();
 
-      res.status(200).send(livroResultados);
-    } catch (erro) {
-      next(erro);
-    }
-  }
+        if (livroResultado !== null) {
+         res.status(200).send(livroResultado);
+        } else {
+         next(new NaoEncontrado("Id do livro não localizado."));
+        }
+      } catch (erro) {
+        next(erro);
+      }
+    };
 
   static cadastrarLivro = async (req, res, next) => {
     try {
@@ -41,29 +46,37 @@ class LivroController {
     }
   }
 
-  static atualizarLivro = async (req, res, next) => {
-    try {
-      const id = req.params.id;
+    static atualizarLivro = async (req, res, next) => {
+      try {
+       const id = req.params.id;
 
-      await livros.findByIdAndUpdate(id, {$set: req.body});
+        const livroResultado = await livros.findByIdAndUpdate(id, {$set: req.body});
 
-      res.status(200).send({message: "Livro atualizado com sucesso"});
-    } catch (erro) {
-      next(erro);
-    }
-  }
+        if (livroResultado !== null) {
+         res.status(200).send({message: "Livro atualizado com sucesso"});
+        } else {
+          next(new NaoEncontrado("Id do livro não localizado."));
+        }
+      } catch (erro) {
+       next(erro);
+      } 
+    };
 
-  static excluirLivro = async (req, res, next) => {
-    try {
-      const id = req.params.id;
+    static excluirLivro = async (req, res, next) => {
+      try {
+        const id = req.params.id;
 
-      await livros.findByIdAndDelete(id);
+        const livroResultado = await livros.findByIdAndDelete(id);
 
-      res.status(200).send({message: "Livro removido com sucesso"});
-    } catch (erro) {
-      next(erro);
-    }
-  }
+        if (livroResultado !== null) {
+          res.status(200).send({message: "Livro removido com sucesso"});
+        } else {
+          next(new NaoEncontrado("Id do livro não localizado."));
+        }
+      } catch (erro) {
+        next(erro);
+      }
+    };
 
   static listarLivroPorEditora = async (req, res, next) => {
     try {
