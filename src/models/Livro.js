2fde99f0160148659c27cfcabf07d56fce2a8f02
MarcosRBjunior <marcosribeirojr@alunos.fho.edu.br>
2026-06-17 12:24:05 -0300

    feat: implement page count range and publisher validation in Livro

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -6,9 +6,11 @@ const livroSchema = new mongoose.Schema(
     titulo: {type: String, required: [true, "O título do livro é obrigatório"]},
     autor: {type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: [true, "O autor é obrigatório"]},
     editora: {type: String, required: [true, "A editora é obrigatória"], enum: { values: ["Dicionario do autodidata", "Bordeslesscoding"], message: "A editora {VALUE} não é válida"}},
-    numeroPaginas: {type: Number, min: [10, "O numero de paginas deve estar entre 10 e 5000 {VALUE}"], max: [5000, "O numero de paginas deve estar entre 10 e 5000 {VALUE}"]},
-  }
-);
+    numeroPaginas: {type: Number, validate: {validator: (valor) => {
+      return valor >= 10 && valor <= 5000;
+      }, message: "O número de páginas deve ser entre 10 e 5000. Valor fornecido: {VALUE}"}
+    }
+  });
 
 const livros= mongoose.model('livros', livroSchema);
 
