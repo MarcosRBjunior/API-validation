4150b45df53637ad1fdf8dc6b74757b83737d9ce
MarcosRBjunior <marcosribeirojr@alunos.fho.edu.br>
2026-06-19 00:10:49 -0300

    feat: implement regex search

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -10,7 +10,7 @@ const livroSchema = new mongoose.Schema(
       return valor >= 10 && valor <= 5000;
       }, message: "O número de páginas deve ser entre 10 e 5000. Valor fornecido: {VALUE}"}
     }
-  });
+  }); 
 
 const livros= mongoose.model('livros', livroSchema);
 
--- src/models/validadorGlobal.js
+++ src/models/validadorGlobal.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.Schema.Types.String.set("validate", {validator: (valor) => valor !== "", message: ({path}) => `O campo '${path}' em branco foi fornecido.` 
+mongoose.Schema.Types.String.set("validate", {
+  validator: (valor) => valor.trim() !== "",
+  message: ({ path }) => `O campo ${path} foi fornecido em branco.`
 });
